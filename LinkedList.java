176385e6a21efdaa7f758e2bdfa7b8cc3be8da5a
surjya <surjyamaniduan@gmail.com>
2021-10-05 21:31:48 +0530

    add:Delete first element

--- LinkedList.java
+++ LinkedList.java
@@ -4,18 +4,25 @@ import java.util.Scanner;
 
 public class LinkedList {
 	public static void main(String[] args) {
-		 
-		System.out.println("Welcome to linkedList DataStructure Program");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter 1 to add the data at the start");
-        System.out.println("Enter 2 to add the data at the end");
-        switch (scanner.nextInt()) {
-            case 1:
-                Operations.addDataAtStart();
-                break;
-            case 2:
-                Operations.addDataAtEnd();
-                break;
-        }
-    }
-}
+		 System.out.println("Welcome to linkedList DataStructure Program");
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("Enter 1 to add the data at the start");
+	        System.out.println("Enter 2 to add the data at the end");
+	        System.out.println("Enter 3 to insert data inBetween");
+	        System.out.println("Enter 4 to delete first element");
+	        switch (scanner.nextInt()) {
+	            case 1:
+	                Operations.addDataAtStart();
+	                break;
+	            case 2:
+	                Operations.addDataAtEnd();
+	                break;
+	            case 3:
+	                Operations.insertInBetween();
+	                break;
+	            case 4:
+	                Operations.deleteFirstNode();
+	                break;
+	        }
+	    }
+	}
