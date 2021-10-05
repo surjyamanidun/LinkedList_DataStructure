9925dd06eb7b20b3ab89318a1211a813f000db20
surjya <surjyamaniduan@gmail.com>
2021-10-05 21:36:03 +0530

    add: Delete Last Element

--- LinkedList.java
+++ LinkedList.java
@@ -4,25 +4,29 @@ import java.util.Scanner;
 
 public class LinkedList {
 	public static void main(String[] args) {
-		 System.out.println("Welcome to linkedList DataStructure Program");
-	        Scanner scanner = new Scanner(System.in);
-	        System.out.println("Enter 1 to add the data at the start");
-	        System.out.println("Enter 2 to add the data at the end");
-	        System.out.println("Enter 3 to insert data inBetween");
-	        System.out.println("Enter 4 to delete first element");
-	        switch (scanner.nextInt()) {
-	            case 1:
-	                Operations.addDataAtStart();
-	                break;
-	            case 2:
-	                Operations.addDataAtEnd();
-	                break;
-	            case 3:
-	                Operations.insertInBetween();
-	                break;
-	            case 4:
-	                Operations.deleteFirstNode();
-	                break;
-	        }
-	    }
-	}
+		System.out.println("Welcome to linkedList DataStructure Program");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 to add the data at the start");
+        System.out.println("Enter 2 to add the data at the end");
+        System.out.println("Enter 3 to insert data inBetween");
+        System.out.println("Enter 4 to delete first element");
+        System.out.println("enter 5 to delete data of last node");
+        switch (scanner.nextInt()) {
+            case 1:
+                Operations.addDataAtStart();
+                break;
+            case 2:
+                Operations.addDataAtEnd();
+                break;
+            case 3:
+                Operations.insertInBetween();
+                break;
+            case 4:
+                Operations.deleteFirstNode();
+                break;
+            case 5:
+                Operations.deleteLastNode();
+                break;
+        }
+    }
+}
